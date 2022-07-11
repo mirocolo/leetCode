af3aa67a2010d10d0ce1805f457fea9afba77984
mirocolo <77186675@qq.com>
2022-07-11 09:37:38 +0800

    feat 问题10 正则表达式匹配

---
+++ src/main/java/question/d/Application10.java
@@ -0,0 +1,70 @@
+package question.d;
+
+/**
+ * @Description: 正则表达式匹配
+ * @Author 何鑫
+ * @Date 2022/7/4 10:04
+ */
+public class Application10 {
+    /**
+     * 给你一个字符串s和一个字符规律p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+     *
+     * '.' 匹配任意单个字符
+     * '*' 匹配零个或多个前面的那一个元素
+     * 所谓匹配，是要涵盖整个字符串s的，而不是部分字符串。
+     *
+     * 输入：s = "aa", p = "a"
+     * 输出：false
+     * 解释："a" 无法匹配 "aa" 整个字符串。
+     *
+     * 输入：s = "aa", p = "a*"
+     * 输出：true
+     * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+     *
+     *输入：s = "ab", p = ".*"
+     * 输出：true
+     * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        char[] cs = s.toCharArray();
+        char[] cp = p.toCharArray();
+
+        // dp[i][j]:表示s的前i个字符，p的前j个字符是否能够匹配
+        boolean[][] dp = new boolean[cs.length + 1][cp.length + 1];
+
+        // 初期值
+        // s为空，p为空，能匹配上
+        dp[0][0] = true;
+        // p为空，s不为空，必为false(boolean数组默认值为false，无需处理)
+
+        // s为空，p不为空，由于*可以匹配0个字符，所以有可能为true
+        for (int j = 2; j <= cp.length; j++) {
+            if (cp[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        // 填格子
+        for (int i = 1; i <= cs.length; i++) {
+            for (int j = 1; j <= cp.length; j++) {
+                // 文本串和模式串末位字符能匹配上
+                if (cs[i - 1] == cp[j - 1] || cp[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (cp[j - 1] == '*') { // 模式串末位是*
+                    // 模式串*的前一个字符能够跟文本串的末位匹配上
+                    if (cs[i - 1] == cp[j - 2] || cp[j - 2] == '.') {
+                        dp[i][j] = dp[i][j - 2]      // *匹配0次的情况
+                                || dp[i - 1][j];     // *匹配1次或多次的情况
+                    } else { // 模式串*的前一个字符不能够跟文本串的末位匹配
+                        dp[i][j] = dp[i][j - 2];     // *只能匹配0次
+                    }
+                }
+            }
+        }
+        return dp[cs.length][cp.length];
+    }
+}
---
+++ src/test/java/question/d/Application10Test.java
@@ -0,0 +1,48 @@
+package question.d;
+
+import junit.framework.LeetCodeTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Description:
+ * @Author 何鑫
+ * @Date 2022/7/4 15:14
+ */
+@RunWith(Parameterized.class)
+public class Application10Test extends LeetCodeTestCase<Application10> {
+
+    private String s;
+    private String p;
+    private Boolean match;
+
+    public Application10Test(String s, String p, Boolean match) {
+        this.s = s;
+        this.p = p;
+        this.match = match;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"a","a",true},
+                {"aa","a",false},
+                {"aa","a*",true},
+                {"ab",".*",true},
+                {"aaab","a*b",true},
+        });
+    }
+
+    @Test
+    public void isMatch() {
+        Boolean res = instance.isMatch(s,p);
+        Assert.assertEquals("正则表达式匹配",match,res);
+    }
+}
